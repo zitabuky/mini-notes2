ff93390373f7b54e7bd49b1a73e72942856afc50
zitabuky <152078182+zitabuky@users.noreply.github.com>
2025-09-28 17:19:43 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mini-notes-v2'; // növelt verzió, hogy biztos frissüljön
+const CACHE = 'mini-notes-v5';
 const ASSETS = [
   './',
   './index.html',
@@ -35,3 +35,4 @@ self.addEventListener('fetch', e => {
     ).catch(() => caches.match('./index.html'))
   );
 });
+
