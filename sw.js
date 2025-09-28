9d4c364759479b67b16c63f8612363375bde34df
zitabuky <152078182+zitabuky@users.noreply.github.com>
2025-09-28 15:06:23 +0200

    Add files via upload
    Első feltöltés

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = 'mini-notes-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const { request } = event;
+  if (request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(request).then(cached =>
+      cached ||
+      fetch(request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(cache => cache.put(request, copy));
+        return res;
+      })
+    ).catch(() => caches.match('/index.html'))
+  );
+});
